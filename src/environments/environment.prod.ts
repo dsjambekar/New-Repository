11e48658df08799fdc8373054d5e56a15bf3254a
dsjambekar <dsjambekar@gmail.com>
2019-04-08 16:29:36 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleLoginProviderId: '939928615748-a4q3gq6pjuhjmptrfu4vanhinqh1i9do.apps.googleusercontent.com'
+  googleLoginProviderId: '341534115873-nclp710qvjvial7e3pqgel110odcrn9s.apps.googleusercontent.com'
 
 };
